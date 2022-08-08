f1b005aa5132db702fd6acf4171c381583ea0fbf
lemon-butter <kim1231401@naver.com>
2022-08-08 23:42:46 +0900

    small fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(4001);
 }
 bootstrap();
+git config --global user.email "kim1231401@naver.com"
+git config --global user.name "lemon-butter"
