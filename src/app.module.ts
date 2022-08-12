5c4fdbd04861f6c7c3d6f1b6027aa867f64c8c9f
lemon-butter <kim1231401@naver.com>
2022-08-12 11:22:13 +0900

    orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,24 +6,35 @@ import { MboModule } from './mbo/mbo.module';
 import { Mbo } from './mbo/entities/mbo.entity';
 import { ObjectivesModule } from './objectives/objectives.module';
 import { ToDoListModule } from './to-do-list/to-do-list.module';
+import ormconfig from './config/ormconfig';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
     MboModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      load:[ormconfig]
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/common/graphql/schema.gql',
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '915502',
-      database: 'mbo',
-      entities: ['dist/**/**/*.entity{.js,.ts}'],
-      synchronize: true, // 배포시에는 컬럼값수정등을 하게될경우 데이터가 날아가므로 배포환경에서는 false
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: [join(__dirname, '/**/*.entity.js')],
+        synchronize: true,
+        logging:true
+      }),
     }),
     ObjectivesModule,
     ToDoListModule,
